54dce83fba7d42bb76f6ffe35077a63ee56dbdeb
tloeber <thomas.loeber73@gmail.com>
2022-10-02 21:00:08 +0000

    Expand startp script.

--- instance_startup_script.sh
+++ instance_startup_script.sh
@@ -1,4 +1,11 @@
-setup-git:
-    git config --global user.name tloeber
-    git config --global user.email thomas.loeber73@gmail.com
-    git clone https://github.com/tloeber/ML-platform-comparisons.git
+git config --global user.name tloeber
+git config --global user.email thomas.loeber73@gmail.com
+cd ~
+
+git clone https://github.com/tloeber/ML-platform-comparisons.git
+
+# Install credential manager to save PAT
+sudo apt-get -y install libsecret-1-0 libsecret-1-dev
+cd /usr/share/doc/git/contrib/credential/libsecret
+sudo make
+git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
