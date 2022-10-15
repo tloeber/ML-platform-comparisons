441c19f67acb7189e9a6fec2f85a9e687f23eec7
tloeber <thomas.loeber73@gmail.com>
2022-10-15 08:52:31 -0400

    Fixes for startup script to run non-interactively

--- instance_startup_script.sh
+++ instance_startup_script.sh
@@ -2,14 +2,14 @@
 # ======
 git config --global user.name tloeber
 git config --global user.email thomas.loeber73@gmail.com
-cd ~
-
-git clone https://github.com/tloeber/ML-platform-comparisons.git
+cd ~ && \
+    git clone https://github.com/tloeber/ML-platform-comparisons.git
 
 # Install credential manager to save PAT
-sudo apt-get -y install libsecret-1-0 libsecret-1-dev
-cd /usr/share/doc/git/contrib/credential/libsecret
-sudo make
+apt-get update
+apt-get -y install libsecret-1-0 libsecret-1-dev
+cd /usr/share/doc/git/contrib/credential/libsecret && \
+    make
 git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
 
 
